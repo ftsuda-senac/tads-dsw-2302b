de71d2f50604ccbadb74176357822961e8a17c50
ftsuda-senac <ftsuda.senac@gmail.com>
2023-10-30 19:35:45 -0300

    Preparação exemplos Spring Data JPA

---
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplos/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package br.senac.tads.dsw.exemplos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${app.imagem-path}")
+    private String imagemPath;
+
+    @Value("${app.imagem-url-prefix}")
+    private String imagemUrlPrefix;
+
+    /**
+     * Define uma URL para acessar um diretório contendo as imagens<br>
+     * Criar o diretório configurado no sistema.<br>
+     * Referência: https://www.baeldung.com/spring-mvc-static-resources
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(imagemUrlPrefix + "/**")
+                .addResourceLocations("file:///" + imagemPath);
+    }
+}
